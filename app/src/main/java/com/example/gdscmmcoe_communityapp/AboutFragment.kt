399c26ce286b3a23dd98ed1ccd4bb4a1f3103b16
Piyush Terkar <terkar.piyush3@gmail.com>
2022-01-08 15:58:28 +0530

    About Page Final commit, Functionality Tested

--- app/src/main/java/com/example/gdscmmcoe_communityapp/AboutFragment.kt
+++ app/src/main/java/com/example/gdscmmcoe_communityapp/AboutFragment.kt
@@ -1,13 +1,20 @@
 package com.example.gdscmmcoe_communityapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.gdscmmcoe_communityapp.databinding.FragmentAboutBinding
+import com.example.gdscmmcoe_communityapp.databinding.FragmentHomeBinding
 
 class AboutFragment : Fragment() {
+    private var _binding: FragmentAboutBinding? = null
 
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,6 +24,34 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        //return inflater.inflate(R.layout.fragment_about, container, false)
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fun OpenLink(url : String) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+        binding.instaLink.setOnClickListener{
+            OpenLink("https://instagram.com/gdsc_mmcoe?utm_medium=copy_link")
+        }
+
+        binding.LinkedinLink.setOnClickListener{
+            OpenLink("https://www.linkedin.com/in/gdsc-mmcoe-b1065b21b")
+        }
+        binding.youtubeLink.setOnClickListener{
+            OpenLink("https://youtube.com/channel/UCLwzfI-P_ommZ_QMDGECmYQ")
+        }
+        binding.webBtn.setOnClickListener{
+            OpenLink("https://gdscmmcoe.netlify.app")
+        }
+        binding.memberbtn.setOnClickListener{
+            OpenLink("https://gdsc.community.dev/marathwada-mitra-mandals-college-of-engineering-pune/")
+        }
+    }
+
 }

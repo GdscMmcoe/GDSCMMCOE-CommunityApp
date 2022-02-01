d21e6d1c0d3e4c8550433940bdc759facb3381a1
Aaditya Paithane <51539518+aadityamp01@users.noreply.github.com>
2022-02-01 18:10:24 +0530

    Updated team adapter

--- app/src/main/java/com/example/gdscmmcoe_communityapp/adapter/TeamAdapter.kt
+++ app/src/main/java/com/example/gdscmmcoe_communityapp/adapter/TeamAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.gdscmmcoe_communityapp.adapter
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -26,6 +27,26 @@ class TeamAdapter: RecyclerView.Adapter<TeamAdapter.TeamViewHolder>() {
     override fun onBindViewHolder(holder: TeamViewHolder, position: Int) {
         holder.binding.tvName.text = oldData[position].Name
         holder.binding.tvRole.text = oldData[position].Role
+
+        for(i in 0..oldData.size step 5){
+            when (position) {
+                i -> {
+                    holder.binding.tvName.setTextColor(Color.parseColor("#4483F3"))
+                }
+                i + 1 -> {
+                    holder.binding.tvName.setTextColor(Color.parseColor("#FBBB04"))
+                }
+                i + 2 -> {
+                    holder.binding.tvName.setTextColor(Color.parseColor("#0C9C5B"))
+                }
+                i + 3 -> {
+                    holder.binding.tvName.setTextColor(Color.parseColor("#EB4434"))
+                }
+                i + 4 -> {
+                    holder.binding.tvName.setTextColor(Color.parseColor("#141115"))
+                }
+            }
+        }
     }
 
     override fun getItemCount(): Int {
